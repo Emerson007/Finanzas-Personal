18019bdee4ccebbdd4c27a8b35d05c454e161bbd
Beito <beito_xp@me.com>
2013-12-17 09:03:09 -0600

    Actualizacion9: Cambio a Dominio Publico

--- Desktop/Finanzas/DataBase/Direccion.php
+++ Desktop/Finanzas/DataBase/Direccion.php
@@ -4,10 +4,14 @@ class Direccion
 
 {
 
-	private $servidor = "localhost";
-	private $baseDatos = "FinanzaPersonal";
-	private $usuario = "root";
-	private $contrasenia = "root";	
+	//private $servidor = "localhost";
+	private $servidor = "sql4.freemysqlhosting.net";
+	//private $baseDatos = "FinanzaPersonal";
+	private $baseDatos = "sql425084";
+	//private $usuario = "root";
+	private $usuario = "sql425084";
+	//private $contrasenia = "root";	
+	private $contrasenia = "tJ8!zD2!";
 	
 	public function getServidor()
 	
